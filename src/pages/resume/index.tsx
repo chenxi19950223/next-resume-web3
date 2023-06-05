21ec89d180501a59d34a33ebbfaea9b64f96cc59
chenxi19950223 <46037778+chenxi19950223@users.noreply.github.com>
2023-06-05 14:21:59 +0800

    页面最小宽度

---
+++ src/pages/resume/index.tsx
@@ -0,0 +1,25 @@
+import '@wangeditor/editor/dist/css/style.css'
+import dynamic from 'next/dynamic'
+
+const Editor = dynamic(() => import('@/components/editor/editor'), {
+    ssr: false
+})
+function Resume () {
+
+
+    return (
+        <>
+            <div className='h-full flex flex-col'>
+                <div className='flex justify-between p-3'>
+                    <button></button>
+                    <button className='h-[30px] w-[80px] rounded-[6px] bg-blue-400 text-white'>保存</button>
+                </div>
+                <div className='flex-1'>
+                    <Editor />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Resume;
